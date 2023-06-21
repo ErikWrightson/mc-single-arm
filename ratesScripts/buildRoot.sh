60789c6fb98873dea49cc530cbcf453cd3e8a41d
Erik Wrightson <wrightso@ifarm1801.jlab.org>
2023-06-21 12:45:05 -0400

    moved scripts to their own directory and modified them accordingly

---
+++ ratesScripts/buildRoot.sh
@@ -0,0 +1,19 @@
+# This shell script will take the name of an input file in the infiles directory and run it through
+# the mc_single_arm simulator and then convert the compressed file to a root file.
+if ( $#argv != 1 ) then
+    echo "This script requires arguments of the name of a input file found in the infiles directory."
+else
+    cd ..
+    set pth = "infiles/$argv[1].inp"
+    if (-e "$pth") then
+        cd src
+        mc_single_arm << endofinput >! ../runout/$argv[1].out
+$argv[1]
+endofinput
+        cd ..
+        source /apps/root/6.10.02/bin/thisroot.csh
+        h2root worksim/$argv[1].rzdat worksim/$argv[1].root
+    else
+        echo "File name provided ($pth) does not exist or is not found in the worksim directory."
+    endif
+endif
